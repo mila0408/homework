830ef91d6266786c280ad8ca8ad293f2ceca6eec
mila0408 <159796730+mila0408@users.noreply.github.com>
2024-03-18 16:59:35 +0300

    Add files via upload

---
+++ homework 312.0.cpp
@@ -0,0 +1,41 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+
+	int min, max; cin >> min >> max;
+	srand(time(NULL));
+	const int A = 10;
+	int array[A];
+	for (int i = 0; i < A; i++) {
+		array[i] = min + rand() % (max - min + 1);
+		cout << array[i] << ' ';
+	}
+	cout << endl;
+
+	bool flag;
+	for (int i = 0; i < A; i++) {
+		flag = true;
+		for (int j = 0; j < A - 1 - i; j++) {
+			if (array[j] > array[j + 1]) {
+				swap(array[j], array[j + 1]);
+				flag = false;
+			}
+		}
+		if (flag)break;
+	}
+
+	for (int i = 0; i < A; i++) {
+		cout << array[i] << ' ';
+	}
+	cout << endl;
+
+
+
+}
