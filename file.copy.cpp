d9c5ec5f91dd2441e304e7fdd32b179fcc7e01b4
mila0408 <159796730+mila0408@users.noreply.github.com>
2024-05-31 21:06:57 +0300

    Add files via upload

---
+++ file.copy.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <Windows.h>
+#include <fstream>
+#include <iostream>
+#include <string.h>
+using namespace std;
+const string  PATH = "copy.txt";
+void copyFile(string path,int size) {
+	ifstream in(path);
+	if (in.is_open())
+	{
+		string str;
+		while (getline(in,str))
+		{
+			if (str.length()<= size)
+			{
+				ofstream out("text.copy.txt");
+				out << str;
+			}
+		}
+	}
+}
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+	int size;
+	cout << "Введите число:" << endl;
+	cin >> size;
+	copyFile(PATH, size);
+	
+
+
+}
+
